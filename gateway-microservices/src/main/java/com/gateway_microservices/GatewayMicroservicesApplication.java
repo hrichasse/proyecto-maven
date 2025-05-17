fe02af5494c6db0c77319355c61ee7c9816c878a
Hernan Richasse <hernanrichasse@gmail.com>
2025-05-17 13:01:50 -0400

    proyecto Maven

---
+++ gateway-microservices/src/main/java/com/gateway_microservices/GatewayMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.gateway_microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayMicroservicesApplication.class, args);
+	}
+
+}
