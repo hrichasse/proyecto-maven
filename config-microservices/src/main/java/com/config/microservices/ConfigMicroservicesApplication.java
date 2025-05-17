6806ffb4e9217a473fafd67b46742b070b93ce45
Hernan Richasse <hernanrichasse@gmail.com>
2025-05-17 13:36:08 -0400

    ultimo

---
+++ config-microservices/src/main/java/com/config/microservices/ConfigMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.config.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigMicroservicesApplication.class, args);
+	}
+
+}
